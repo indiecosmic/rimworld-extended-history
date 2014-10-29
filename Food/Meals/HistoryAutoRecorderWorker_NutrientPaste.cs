05fb96bf9ac510f58b66a9b00c53d9a10e475130
Mattias Rudén <mattias@indiesoft.org>
2014-10-29 23:39:37 +0100

    Refactored, updated version numbers.

---
+++ Food/Meals/HistoryAutoRecorderWorker_NutrientPaste.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace IndieSoft.RimWorld.ExtendedHistory.Food
+{
+    public class HistoryAutoRecorderWorker_NutrientPaste : HistoryAutoRecorderWorker_MealBase
+    {
+        public HistoryAutoRecorderWorker_NutrientPaste()
+            : base(FoodQuality.MealAwful)
+        {
+
+        }
+    }
+}
